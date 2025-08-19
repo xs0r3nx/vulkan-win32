eddb8cbee49968b7f7873ac7683db68e7c055aa2
xs0r3nx <s0r3n85@gmail.com>
2025-08-19 20:27:46 +0330

    added shader support. see those spvz!

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <algorithm> // Necessary for std::clamp
 #include <limits> // Necessary for std::numeric_limits
 #include <cstdint> // Necessary for uint32_t
+#include <fstream>
 #include "resource.h"
 const wchar_t CLASS_NAME[] = L"VulkanWindow";
 const std::vector<const char*> validationLayers =
@@ -83,7 +84,7 @@ public:
 	{
 		initConsole();
 		initWindow(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
-		initVulkan(hInstance,hwnd);
+		initVulkan(hInstance, hwnd);
 		mainLoop();
 		cleanup();
 	}
@@ -164,15 +165,17 @@ private:
 
 
 	}
-	void initVulkan(HINSTANCE hInstance,HWND hwnd)
+	void initVulkan(HINSTANCE hInstance, HWND hwnd)
 	{
 		createInstance();
 		setupDebugMessenger();
-		createSurface(hInstance,hwnd);
+		createSurface(hInstance, hwnd);
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain();
 		createImageViews();
+		createGraphicsPipeline();
+
 	}
 	void mainLoop()
 	{
@@ -417,7 +420,7 @@ private:
 		vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
 		vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
 	}
-	void createSurface(HINSTANCE hInstance,HWND hwnd)
+	void createSurface(HINSTANCE hInstance, HWND hwnd)
 	{
 		VkWin32SurfaceCreateInfoKHR createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
@@ -552,8 +555,53 @@ private:
 			}
 		}
 	}
+	void createGraphicsPipeline()
+	{
+		auto vertShaderCode = readFile("vert.spv");
+		auto fragShaderCode = readFile("frag.spv");
+		VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+		VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+		VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+		vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+		vertShaderStageInfo.module = vertShaderModule;
+		vertShaderStageInfo.pName = "main";
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+		fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+		fragShaderStageInfo.module = fragShaderModule;
+		fragShaderStageInfo.pName = "main";
+		VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
 
+		vkDestroyShaderModule(device, fragShaderModule, nullptr);
+		vkDestroyShaderModule(device, vertShaderModule, nullptr);
+	}
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+
+		return buffer;
+	}
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		VkShaderModuleCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+		VkShaderModule shaderModule;
+		if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create shader module!");
+		}
+		return shaderModule;
+
+	}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
